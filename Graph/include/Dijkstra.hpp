3beb071fd36cd245dafd9865cb4394372d797501
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-04-21 23:36:17 +0200

    Encapsulate algorithm into class

--- Graph/include/Dijkstra.hpp
+++ Graph/include/Dijkstra.hpp
@@ -1,7 +1,82 @@
 #ifndef ADVANCED_OPTIMIZATION_METHODS_DIJKSTRA_HPP
 #define ADVANCED_OPTIMIZATION_METHODS_DIJKSTRA_HPP
 
-template<typename Distance>
-class Dijkstra {};
+#include <algorithm>
+#include <limits>
+#include <set>
+#include <tuple>
+#include <vector>
+
+template<typename T>
+using Matrix = std::vector<std::vector<T>>;
+
+template<typename T>
+class Dijkstra {
+public:
+    Dijkstra(const Matrix<T>& graph,
+             std::size_t source,
+             std::size_t destination) :
+        graph_{graph},
+        source_{source},
+        destination_{destination},
+        minDists_{graph_.size(), std::numeric_limits<T>::max()} {
+        Init();
+        Run();
+    }
+
+    Dijkstra() = delete;
+    ~Dijkstra() = default;
+
+    Dijkstra(const Dijkstra& other) = delete;
+    Dijkstra& operator=(const Dijkstra& other) = delete;
+
+    Dijkstra(Dijkstra&& other) noexcept = default;
+    Dijkstra& operator=(Dijkstra&& other) noexcept = default;
+
+    std::tuple<T, std::vector<std::size_t>> GetResult() {
+        return {desiredDistance_, route_};
+    }
+
+    void Run() {
+        while (!activeVertices_.empty()) {
+            auto [minimalDistance, currentVertex] = *activeVertices_.begin();
+            if (currentVertex == destination_) {
+                desiredDistance_ = minimalDistance;
+                return;
+            }
+            activeVertices_.erase(activeVertices_.begin());
+            EvaluateCurrentVertex(currentVertex);
+        }
+        desiredDistance_ = std::numeric_limits<std::size_t>::max();
+        route_ = {};
+    }
+
+private:
+    Matrix<T> graph_;
+    std::size_t source_;
+    std::size_t destination_;
+
+    std::vector<T> minDists_{};
+    std::set<std::pair<T, std::size_t>> activeVertices_{};
+
+    std::vector<std::size_t> route_{};
+    T desiredDistance_{};
+
+    void Init() {
+        minDists_[source_] = 0;
+        activeVertices_.insert({0, source_});
+    }
+
+    void EvaluateCurrentVertex(std::size_t vertex) {
+        const auto& dists = graph_[vertex];
+        for (std::size_t i = 0; i < dists.size(); i++) {
+            if (dists[i] && minDists_[i] > minDists_[vertex] + dists[i]) {
+                activeVertices_.erase({minDists_[i], i});
+                minDists_[i] = minDists_[vertex] + dists[i];
+                activeVertices_.insert({minDists_[i], i});
+            }
+        }
+    }
+};
 
 #endif // ADVANCED_OPTIMIZATION_METHODS_DIJKSTRA_HPP
--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,48 +1,17 @@
 #include "Dijkstra.hpp"
 #include <iostream>
-#include <limits>
-#include <set>
-#include <vector>
-
-std::size_t dijkstra(const std::vector<std::vector<std::size_t>>& graph,
-                     std::size_t source,
-                     std::size_t target) {
-    std::vector<std::size_t> min_distance(
-        graph.size(),
-        std::numeric_limits<std::size_t>::max());
-    min_distance[source] = 0;
-    std::set<std::pair<std::size_t, std::size_t>> active_vertices;
-    active_vertices.insert({0, source});
-
-    while (!active_vertices.empty()) {
-        std::size_t where = active_vertices.begin()->second;
-        if (where == target) {
-            return min_distance[where];
-        }
-        active_vertices.erase(active_vertices.begin());
-        std::size_t i = 0;
-        for (auto ed : graph[where]) {
-            if (ed && min_distance[i] > min_distance[where] + ed) {
-                active_vertices.erase({min_distance[i], i});
-                min_distance[i] = min_distance[where] + ed;
-                active_vertices.insert({min_distance[i], i});
-            }
-            i++;
-        }
-    }
-    return std::numeric_limits<std::size_t>::max();
-}
 
 int main() {
-    auto dd = dijkstra({{0, 5, 3, 0, 0, 0, 0},
-                        {0, 0, 2, 0, 3, 0, 1},
-                        {0, 0, 0, 7, 7, 0, 0},
-                        {2, 0, 0, 0, 0, 6, 0},
-                        {0, 0, 0, 2, 0, 1, 0},
-                        {0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 1, 0, 0}},
-                       0,
-                       3);
-    std::cout << dd;
+    Dijkstra<std::size_t> dijkstra({{0, 5, 3, 0, 0, 0, 0},
+                                    {0, 0, 2, 0, 3, 0, 1},
+                                    {0, 0, 0, 7, 7, 0, 0},
+                                    {2, 0, 0, 0, 0, 6, 0},
+                                    {0, 0, 0, 2, 0, 1, 0},
+                                    {0, 0, 0, 0, 0, 0, 0},
+                                    {0, 0, 0, 0, 1, 0, 0}},
+                                   0,
+                                   3);
+    auto [distance, route] = dijkstra.GetResult();
+    std::cout << distance;
     return 0;
 }
