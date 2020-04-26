704c93d8aed944940293b6053d49a031f25e0987
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-04-26 16:43:04 +0200

    Add demo version of the algorithm

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,13 +1,27 @@
 #include "Dijkstra.hpp"
 #include "GenericOstreamOperators.hpp"
 #include "Model.hpp"
+#include <algorithm>
 #include <iostream>
 
 int main() {
     AOM::Model<std::size_t> rawModel{0};
     auto model = rawModel.GetModel();
-    AOM::Dijkstra<std::size_t> dijkstra{model, 14, 0};
-    auto [distance, route] = dijkstra.GetResult();
-    std::cout << "route: " << route << ", distance: " << distance << std::endl;
+    using SrcDestVector = std::vector<std::pair<std::size_t, std::size_t>>;
+    SrcDestVector srcDestVec{{14, 0},
+                             {0, 14},
+                             {4, 5},
+                             {3, 3},
+                             {10, 2},
+                             {0, 0},
+                             {12, 30}};
+    std::for_each(srcDestVec.begin(), srcDestVec.end(), [&](const auto& pair) {
+        const auto [source, destination] = pair;
+        AOM::Dijkstra<std::size_t> dijkstra{model, source, destination};
+        auto [distance, route] = dijkstra.GetResult();
+        std::cout << "source: " << source << ", destination: " << destination
+                  << "\nroute: " << route << ", distance: " << distance
+                  << "\n\n";
+    });
     return 0;
 }
