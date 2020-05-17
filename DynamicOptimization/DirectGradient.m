210d9eef374d925c8bb4a048f7c7bd5381137abf
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-05-17 18:41:44 +0200

    Add direct gradient method

---
+++ DynamicOptimization/DirectGradient.m
@@ -0,0 +1,55 @@
+clear all
+close all
+clc
+
+%% Init
+K = 15;
+N = 7;
+eps = 0.25;
+
+A = 0.6;
+B = 0.9;
+Q = 7;
+H = 2;
+F = 4;
+
+u = zeros(K, N);
+u(1, :) = [0.1, 0.4, 0.3, 0.5, 0.2, 0.1, 0.2];
+x = zeros(K, N + 1);
+x(:, 1) = 0.6;
+p = zeros(1, N + 1);
+b = zeros(1, N);
+J = zeros(K, 1);
+t = 0.1;
+
+for iter = 1 : K
+    for i = 2 : size(x, 2)
+        x(iter, i) = A * x(iter, i - 1) ^ 2 + B * u(iter, i - 1);
+    end
+    p(end) = 2 * F * x(end) ^ 3;
+    for i = N : -1 : 1
+        p(i) = 2 * x(iter,i) * (Q + p(i + 1) * A);
+    end
+    for i = 1 : length(b)
+        b(i) = 2 * H * u(iter, i) + B * p(i + 1);
+    end
+    if norm(b) < eps
+        break;
+    end
+    J(iter) = sum(Q * x(iter, 1 : end - 1) .^ 2 + H * u(iter, :) .^ 2) + ...
+              F / 2 * x(end) ^ 4;
+    u(iter + 1, :) = u(iter, :) - t * b;
+end
+
+%% Plot
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k'];
+figure;
+for i = 2 : N + 1
+    plot(x(1 : iter, i), colors(i - 1));
+    hold on;
+end
+title('Trajectories, direct gradient');
+
+figure;
+plot(J(1 : iter));
+title('Performance index');
